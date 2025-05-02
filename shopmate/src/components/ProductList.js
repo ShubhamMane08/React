5381e6504b66c73f8b08eb3c3901fc47fff8b6cd
Shubham Mane <shubhammanecr7@gmail.com>
2025-05-02 15:32:47 +0530

    Changes

--- shopmate/src/components/ProductList.js
+++ shopmate/src/components/ProductList.js
@@ -1,12 +1,15 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export const ProductList = () => {
   const [products, setProduct]=useState([]);
 
   console.log(products);
-  fetch("http://localhost:8000/products")
-  .then(response => response.json)
-  .then(data => console.log(data));
+ 
+  useEffect(() => {
+  
+   
+  },[]
+)
   
     return (
     <div>ProductList</div>
