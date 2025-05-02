89af6ecb5406fffa394cf21aaaefe962de8b5875
Shubham Mane <shubhammanecr7@gmail.com>
2025-05-02 14:55:59 +0530

    creating productList

--- shopmate/src/App.js
+++ shopmate/src/App.js
@@ -1,9 +1,9 @@
-
 import './App.css';
+import { ProductList } from './components/ProductList';
 function App() {
   return (
     <div className="App">
-      <h2>Hello World</h2>
+      <ProductList/>
     </div>
   );
 }
