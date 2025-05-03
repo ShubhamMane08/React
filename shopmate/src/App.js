71b2d9ccf008e3d4f99c48b0a5812e8e5dcc15c0
Shubham Mane <shubhammanecr7@gmail.com>
2025-05-03 12:07:29 +0530

    changes in the CSS

--- shopmate/src/App.js
+++ shopmate/src/App.js
@@ -3,7 +3,8 @@ import { ProductList } from './components/ProductList';
 function App() {
   return (
     <div className="App">
-      <h1>Product List</h1>
+      <h1>Product List</h1><br />
+      <br />
       <ProductList/>
     </div>
   );
--- shopmate/src/components/ProductList.js
+++ shopmate/src/components/ProductList.js
@@ -1,17 +1,33 @@
 import { useEffect, useState } from "react"
-
+import "./ProductList.css";
 export const ProductList = () => {
-  const [products, setProduct]=useState([]);
+  const [products, setProducts]=useState([]);
 
   console.log(products);
  
   useEffect(() => {
-  
-   
+
+    fetch("http://localhost:8000/products")
+    .then(response => response.json())
+    .then(data => setProducts(data)); 
   },[]
-)
-  
-    return (
-    <div>ProductList</div>
+);
+return (
+      <section>
+          {products.map((product)=>(
+            <div className="card" key={product.id}>
+              <p className="id">{product.id}</p>
+              <p className="name">{product.name}</p>
+              <p>
+                <span className="info">
+                  ${product.price}
+                  </span>
+                  <span className={product.in_stock? "instock":"unavailable"}>
+                    {product.in_stock? "In stock":"Out of Stock"}
+                  </span>
+              </p>
+            </div>
+          ))}
+      </section>    
   )
 }
