3a16419f98936ad054f00d5f2384c984af32f0dd
Shubham Mane <shubhammanecr7@gmail.com>
2025-05-03 12:16:37 +0530

    displaying details through fetching data

--- shopmate/src/components/ProductList.js
+++ shopmate/src/components/ProductList.js
@@ -2,18 +2,21 @@ import { useEffect, useState } from "react"
 import "./ProductList.css";
 export const ProductList = () => {
   const [products, setProducts]=useState([]);
+  const [url, setUrl]=useState("http://localhost:8000/products");
 
   console.log(products);
  
   useEffect(() => {
 
-    fetch("http://localhost:8000/products")
+    fetch(url)
     .then(response => response.json())
     .then(data => setProducts(data)); 
   },[]
 );
 return (
       <section>
+        <button onClick={()=>setUrl("")}>All</button>
+        <button onClick={()=>setUrl}></button>
           {products.map((product)=>(
             <div className="card" key={product.id}>
               <p className="id">{product.id}</p>
