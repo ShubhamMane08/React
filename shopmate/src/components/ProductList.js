9c1cfece607a78120004c81e292dd93a9b1ca6c0
Shubham Mane <shubhammanecr7@gmail.com>
2025-05-03 13:52:19 +0530

    changes

--- shopmate/src/components/ProductList.js
+++ shopmate/src/components/ProductList.js
@@ -1,28 +1,31 @@
 import { useCallback, useEffect, useState } from "react"
 import "./ProductList.css";
-export const ProductList = () => {
-  const [products, setProducts]=useState([]);
-  const [url, setUrl]=useState("http://localhost:8000/products");
+import { useFetch } from "../hooks/useFetch";
 
-  console.log(products);
+export const ProductList = () => {
+  //const [products, setProducts]=useState([]);
+  const [url, setUrl]=useState("http://localhost:8000/products");
+  const {data : products}= useFetch(url);
+
+//   console.log(products);
  
-  const fetchProducts= useCallback( async () =>{
-    const response = await fetch(url);
-    const data = await response.json();
-    setProducts(data);
-  },[url]) 
+//   const fetchProducts= useCallback( async () =>{
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     setProducts(data);
+//   },[url]) 
  
-  useEffect(() => {
-    fetchProducts();
-  },[fetchProducts]
-);
+//   useEffect(() => {
+//     fetchProducts();
+//   },[fetchProducts]
+// );
 return (
       <section>
         <div className="filter">
           <button onClick={()=>setUrl("http://localhost:8000/products")}>All</button>
           <button onClick={()=>setUrl("http://localhost:8000/products?in_stock=true")}>In Stock Only</button>
         </div>
-          {products.map((product)=>(
+          {products && products.map((product)=>(
             <div className="card" key={product.id}>
               <p className="id">{product.id}</p>
               <p className="name">{product.name}</p>
