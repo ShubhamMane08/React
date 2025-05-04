f16ddc41a8ab94ceb8dafa0cef18b69278816b5c
Shubham Mane <shubhammanecr7@gmail.com>
2025-05-04 18:53:37 +0530

    loading

--- shopmate/src/hooks/useFetch.js
+++ shopmate/src/hooks/useFetch.js
@@ -6,8 +6,10 @@ export const useFetch = (url) => {
     const [loading, setLoading]=useState(false);
     useEffect(()=>{
         const fetchData=async ()=>{
+            setLoading(true);
             const response = await fetch(url);
             const result = await response.json();
+            setLoading(false);
             setData(result);
         }
         fetchData();
