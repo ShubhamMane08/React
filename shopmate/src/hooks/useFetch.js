82421f0eef8268a6aa3f43c4fcbdb528b27fb3fb
Shubham Mane <shubhammanecr7@gmail.com>
2025-05-05 22:46:23 +0530

    Changes

--- shopmate/src/hooks/useFetch.js
+++ shopmate/src/hooks/useFetch.js
@@ -12,7 +12,7 @@ export const useFetch = (url) => {
                 const response = await fetch(url);
                 if(!response.ok)
                 {
-                    throw Error(response.statusText)
+                    throw new Error(response.statusText)
                 }
                 const result = await response.json();
                 console.log(response);
