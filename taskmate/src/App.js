5acac1fde2118972bda106050d3af3fa6b9f109a
Shubham Mane <shubhammanecr7@gmail.com>
2025-05-07 12:14:46 +0530

    Adding the components in Component folder

--- taskmate/src/App.js
+++ taskmate/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-
+import {Header} from "./components/Header.js";
+import {AddTask} from "./components/AddTask.js";
+import {ShowTask} from "./components/ShowTask.js";
 function App() {
   return (
     <div className="App">
