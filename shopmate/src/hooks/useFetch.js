d909d7e437095db8efbc72f0f8a31f913f57b43c
Shubham Mane <shubhammanecr7@gmail.com>
2025-05-07 11:30:44 +0530

    changes

--- shopmate/src/hooks/useFetch.js
+++ shopmate/src/hooks/useFetch.js
@@ -1,40 +1,35 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react";
 
-export const useFetch = (url,_body) => {
+export const useFetch = (url, _body) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const body = useRef(_body);
 
-    const [data,setData]=useState(null);
-    const [loading, setLoading]=useState(false);
-    const [error,setError]=useState("");
-    const body = useRef(_body);
+  useEffect(() => {
+    const controller = new AbortController(); // Moved here
 
-    useEffect(()=>{
-        const fetchData=async ()=>{
-            setLoading(true);
-            try
-            {
-                const controller = new AbortController();
-                const response = await fetch(url,{signal:controller.signal});
-                if(!response.ok)
-                {
-                    throw new Error(response.statusText)
-                }
-                const result = await response.json();
-                console.log(response);
-                setLoading(false);
-                setData(result);
-            }
-            catch(error)
-            {
-        
-                setError(error.message);
-                setLoading(false);
-                setError("");
-            }
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(url, { signal: controller.signal });
+        if (!response.ok) {
+          throw new Error(response.statusText);
         }
-        fetchData();
-        return (()=> controller.abort());
-    },[url,body]);
+        const result = await response.json();
+        console.log(response);
+        setData(result);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    
-  return {data, loading,error}
-}
+    fetchData();
+
+    return () => controller.abort();
+  }, [url, body]);
+
+  return { data, loading, error };
+};
