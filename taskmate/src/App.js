f45ad1687c37a7d754bcf970f226be2adf40cb42
Shubham Mane <shubhammanecr7@gmail.com>
2025-05-08 21:45:14 +0530

    changes

--- taskmate/src/App.js
+++ taskmate/src/App.js
@@ -3,6 +3,9 @@ import {Header} from "./components/Header.js";
 import {AddTask} from "./components/AddTask.js";
 import {ShowTask} from "./components/ShowTask.js";
 function App() {
+
+  const [tasklist,setTasklist]=useState([]);
+
   return (
     <div className="App">
       <Header/>
