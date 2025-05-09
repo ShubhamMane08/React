450c360420f0d08db94bfe724b3c4a86a914cc82
Shubham Mane <shubhammanecr7@gmail.com>
2025-05-09 20:50:22 +0530

    changes

--- taskmate/src/App.js
+++ taskmate/src/App.js
@@ -7,12 +7,19 @@ import {ShowTask} from "./components/ShowTask.js";
 function App() {
 
   const [tasklist,setTasklist]=useState([]);
+  const [task,setTask]=useState({});
 
   return (
     <div className="App">
       <Header/>
-      <AddTask tasklist={tasklist} setTasklist={setTasklist}/>
-      <ShowTask tasklist={tasklist} setTasklist={setTasklist}/>
+      <AddTask tasklist={tasklist} 
+      setTasklist={setTasklist}
+      task = {task}
+      setTask={setTask}/>
+      <ShowTask tasklist={tasklist} 
+      setTasklist={setTasklist}
+      task = {task}
+      setTask={setTask}/>
     </div>
   );
 }
--- taskmate/src/components/AddTask.js
+++ taskmate/src/components/AddTask.js
@@ -1,19 +1,20 @@
-export const AddTask = ({tasklist,setTasklist}) => {
+export const AddTask = ({tasklist,setTasklist,task,setTask}) => {
   const handleSubmit=(e)=>{
-    e.preventDefault();
+   e.preventDefault();
 
     const date = new Date();
     const newTask = {id:date.getTime(),
       name:e.target.task.value,
-      time:`${date.toLocaleTimeString} ${date.toLocaleDateString}`
+      time:`${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
     };
     setTasklist([...tasklist,newTask]);
+    e.target.task.value="";
 
   }
     return (
       <section className="addTask">
           <form onSubmit={handleSubmit}>
-              <input type="text" name="task" autoComplete="off" placeholder="add task" maxLength="25" />
+              <input type="text" name="task" value={task.name} autoComplete="off" placeholder="add task" maxLength="25" />
               <button type="submit">Add</button>
           </form>
       </section>
