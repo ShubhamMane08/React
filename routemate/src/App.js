c5f9ee41cfb4742f4176c9e6095917c1b1e96d82
Shubham Mane <shubhammanecr7@gmail.com>
2025-05-11 16:47:53 +0530

    Changes

--- routemate/src/App.js
+++ routemate/src/App.js
@@ -2,16 +2,17 @@ import './App.css';
 import {Routes,Route} from "react-router-dom"
 import { ProductList } from './components/ProductList';
 import { ProductDetails } from './components/ProductDetails';
+import {Home} from "./components/Home";
+
 function App() {
   return (
   
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home/>}>
-        <Route path="products" element={<ProductList/>}>
-        <Route path="products/1" element={<ProductDetails/>}>
+        <Route path="/" element={<Home/>}> </Route>
+        <Route path="products" element={<ProductList/>}> </Route>
+        <Route path="products/1" element={<ProductDetails/>}> </Route>
 
-        </Route>
 
       </Routes>
       
