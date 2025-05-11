cd8d6e4560c53ce9b36d1a2e210230cc092b2f15
Shubham Mane <shubhammanecr7@gmail.com>
2025-05-11 16:34:37 +0530

    Changes

--- routemate/src/App.js
+++ routemate/src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
-    <Bro></Bro>
+  
     <div className="App">
       <h2>Hello World</h2>
       
