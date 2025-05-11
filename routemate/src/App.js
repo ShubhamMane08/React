e89229149c1da5da85279d73dc3b70402aa43c29
Shubham Mane <shubhammanecr7@gmail.com>
2025-05-11 16:50:46 +0530

    Changes

--- routemate/src/App.js
+++ routemate/src/App.js
@@ -14,8 +14,7 @@ function App() {
         <Route path="products" element={<ProductList/>}> </Route>
         <Route path="products/1" element={<ProductDetails/>}> </Route>
         <Route path="contact" element={<Contact/>}> </Route>
-
-
+        
       </Routes>
       
     </div>
