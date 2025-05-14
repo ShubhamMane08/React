d230643b0c845d9ba4c12e95679ee03cb49d7a00
Shubham Mane <shubhammanecr7@gmail.com>
2025-05-14 12:49:01 +0530

    changes

--- routemate/src/App.js
+++ routemate/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
   
     <div className="App">
-      
+
       <header>HEADER</header>
       <Routes>
 
@@ -20,6 +20,7 @@ function App() {
 
       </Routes>
       <footer>FOOTER</footer>
+      
     </div>
   );
 }
