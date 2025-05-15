061aaebdc043402384f9b9ab2eda7088b3b166e1
Shubham Mane <shubhammanecr7@gmail.com>
2025-05-15 21:32:42 +0530

    changes

--- routemate/src/components/Header.js
+++ routemate/src/components/Header.js
@@ -1,5 +1,18 @@
+import Logo from "../../assests/logo.png";
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <a href="/" className="logo">
+      <img src={Logo} alt="Routemate Logo" />
+      <span>Routemate</span>
+      </a>
+      <nav className="navigation">
+        <a href="/">Home</a>
+        <a href="/products">Products</a>
+        <a href="/contact">Contact</a>
+
+      </nav>
+
+    </header>
   )
 }
