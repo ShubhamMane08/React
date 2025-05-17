79b0a5f789c1ea482f474fdd3949af6da2d81d76
Shubham Mane <shubhammanecr7@gmail.com>
2025-05-17 14:48:48 +0530

    changes in the contact page to go back to the Home page

--- routemate/src/components/Contact.js
+++ routemate/src/components/Contact.js
@@ -1,8 +1,16 @@
+import { useNavigate } from "react-router-dom";
 export const Contact = () => {
+  
+    const navigate = useNavigate();
+    const handleSubmit=()=>{
+      console.log("----------------");
+      navigate("/");
+    }
+  
   return (
-
     <main>
     <div className="component">Contact</div>
+    <button onClick={handleSubmit}>Back To Homepage</button>
     </main>
     
   )
