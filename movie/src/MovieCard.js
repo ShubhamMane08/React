dea499a3c7893e8ac7a0591468f3057441298493
Shubham Mane <shubhammanecr7@gmail.com>
2025-05-18 14:56:52 +0530

    Changes

---
+++ movie/src/MovieCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MovieCard({ movie }) {
+  return (
+    <div className="movie-card">
+      <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/150'} alt={movie.Title} />
+      <h3>{movie.Title}</h3>
+      <p>{movie.Year}</p>
+    </div>
+  );
+}
+
+export default MovieCard;
